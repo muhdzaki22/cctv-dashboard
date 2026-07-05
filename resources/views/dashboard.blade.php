cfdfbf8a720327d370f483bd80fd25284612c036
zaki <zaki@websight.com.my>
2026-07-05 12:59:38 +0800

    update chart.js

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -177,7 +177,7 @@
 </div>
 
 @push('scripts')
-<script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.7/dist/chart.umd.min.js"></script>
 <script>
 function dashboard() {
     return {
